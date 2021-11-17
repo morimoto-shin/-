14cdeb79152f7209b960dd90f340565eea5aa980
Okazaki <yo533122@gmail.com>
2021-11-17 15:56:30 +0900

    データの追加と問題形式の変更

--- 問題.py
+++ 問題.py
@@ -1,59 +1,56 @@
 import random
+import re
+import itertools
 import pandas as pd
 import nltk
 import spacy
 import requests
 from bs4 import BeautifulSoup
 
-# スクレイピングでデータ取得
-url = 'https://www.bbc.com/news/science-environment-59253928'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+# コーパスからテキスト取得
+df = pd.read_excel('SCoRE.xls')
 
-texts = soup.find_all('div', class_="ssrcss-uf6wea-RichTextComponentWrapper e1xue1i86")
-all_text = []
-for text in texts:
-    all_text.append(text.text)
-text = ''.join(all_text)
-
-# 文章ごとのリストを作る
-nlp = spacy.load('en_core_web_sm')
-spacy_text = nlp(text)
+# 文書ごとのリストを作る
 sentences = []
-for sent in spacy_text.sents:
-    sentences.append(sent)
-    
+japanese = []
+# {}を取り除く
+for sentence, jp in zip(df['用例'].values, df['日本語訳'].values):
+    sentences.append(''.join(re.split('[{}]', sentence)))
+    japanese.append(jp)
+
 # 文書ごとに分かち書き
-words = nltk.word_tokenize(text)
+word = []
+for text in sentences:
+    word.append(nltk.word_tokenize(text))
+words = list(itertools.chain.from_iterable(word))
 
 # 単語ごとの品詞の取得
 pos = nltk.pos_tag(words)
 
 # 単語と品詞がペアになったDataFrameの作成
-df = pd.DataFrame()
-df['word'] = None
-df['pos_tag'] = None
-for p in pos:
-    df = df.append({'word': p[0], 'pos_tag': p[1]}, ignore_index=True)
-    
-    
-# 問題    
+pos_df = pd.read_csv('word_pos.csv')
+
+
+# 問題
 
 print('\n次の文章の空欄に当てはまる単語を選択肢の中から選び、数字で答えなさい。')
 
 cnt = 0
 correct_num = 0
 while(1):
-    cnt+=1
+    cnt += 1
     import random
     # 問題となる文をランダムに取得
-    question_sent = str(random.choice(sentences))
+    question_sent_idx = random.choice(range(len(sentences)))
+    question_sent = sentences[question_sent_idx]
+    japanese_sent = japanese[question_sent_idx]
 
     while(1):
         # 問題文を単語ごとに分割
         question_sent_words = nltk.word_tokenize(question_sent)
         # 問題文を単語ごとに分割した時のリストからランラムにインデックス番号を取得
-        question_word_index = random.choice(range(len(nltk.word_tokenize(question_sent))))
+        question_word_index = random.choice(
+            range(len(nltk.word_tokenize(question_sent))))
         # 取得したインデックス番号に対応する単語を取得
         question_word = question_sent_words[question_word_index]
 
@@ -72,14 +69,15 @@ while(1):
     # 問題文の作成
     question_sent_words[question_word_index] = ' ____ '
     question = ' '.join(question_sent_words)
-    print(f'Question{cnt}:')
+    print(f'Question{cnt}: {japanese_sent}')
     print(question)
     print('----------------------------------------------------------')
 
-
     # 同じ品詞のものを取得
     while(1):
-        wrong_answer = random.sample(df.loc[df['pos_tag'] == question_pos, 'word'].values.tolist(), 3)
+        wrong_answer = random.sample(
+            pos_df.loc[pos_df['pos_tag'] == question_pos, 'word'].values.tolist(), 3)
+
         def has_duplicates(seq):
             return len(seq) != len(set(seq))
         if answer in wrong_answer:
