2c2abb0cc3c8690e3a11de10e1c4a761b54d33c2
森元心 <shinshinshin0914@gmail.com>
2021-12-15 17:26:05 +0900

    合わせた

--- main/views.py
+++ main/views.py
@@ -1,56 +1,45 @@
-from django.shortcuts import render, redirect
+import pandas as pd
+from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from .question_system import Question
+from .models import Sentence, Pos
+
 
 class IndexView(TemplateView):
-  template_name = 'main/index.html'
+    template_name = 'main/index.html'
+
 
 class QuestionView(View):
-  def get(self, request, *arg, **kwargs):
-    question = {}
-    answer = 1
-    self.answer = answer
-    return render(request, 'main/question.html', {
-      'question': question,
-    })
-    
-  def post(self, request, *args, **kwargs):
-    if 'btn_1' in request.POST:
-      input_answer = 1
-      if self.answer == input_answer:
-        return redirect('/main/answer')
-      else:
-        return redirect('/main/wrong')
-    elif 'btn_2' in request.POST:
-      input_answer = 2
-      if self.answer == input_answer:
-        return redirect('/main/answer')
-      else:
-        return redirect('/main/wrong')
-    elif 'btn_3' in request.POST:
-      input_answer = 3
-      if self.answer == input_answer:
-          return redirect('/main/answer')
-      else:
-        return redirect('/main/wrong')
-    elif 'btn_4' in request.POST:
-      input_answer = 4
-      if self.answer == input_answer:
-          return redirect('/main/answer')
-      else:
-        return redirect('/main/wrong')
+    def get(self, request, *arg, **kwargs):
+        sentence = Sentence  # 英語文と日本語訳のペア
+        pos = Pos  # 品詞と単語のペア
+        # questionインスタンスの作成
+        question = Question(sentence, pos)
+        # 英語文と日本語訳がペアになったDataFrameの作成
+        df = question.create_sentence_df()
+        # データ加工して、英語文と日本語訳に分けたもの
+        english, japanese = question.create_data()
+        # 単語と品詞ペアのDataFrame
+        pos_df = question.create_pos_df()
 
-    return render(request, 'main/question.html', {
-    })
+        print('英語文: ', english[0])
+        print('日本語文: ', japanese[0])
+        print('単語, 品詞: ', pos_df['単語'][0], pos_df['品詞'][0])
 
+        english_question, japanese_question, answers_list, answer, answer_index, question_pos = question.question(
+            english, japanese, pos_df)
+        print('英語問題文: ', english_question)
+        print('日本語問題文: ', japanese_question)
+        print('答えの選択肢: ', answers_list)
+        print('答えの番号: ', answer_index)
+        print('答え: ', answer)
+        print('答えの品詞: ', question_pos)
 
-class AnswerView(View):
-  def post(self, request, *args, **kwargs):
-    return render(request, 'main/answer.html', {
-      ''
-    })
-    
-class WrongAnswerView(View):
-  def post(self, request, *args, **kwargs):
-    return render(request, 'main/wrong_answer.html', {
-      ''
-    })
+        return render(request, 'main/question.html', {
+            'question': sentence
+        })
+
+    def request(self, request, *arg, **kwargs):
+        return render({
+
+        })
